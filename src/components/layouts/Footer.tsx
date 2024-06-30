31a4450f43af5b6500a68800aba53bb372a5197a
Daichi Uematsu <minimi.904.horo@gmail.com>
2024-07-01 01:17:10 +0900

    fix: Deleted unnecessary tailwind class

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,3 +1,3 @@
 export default function Footer({ children }: Readonly<{ children: React.ReactNode }>) {
-    return <div className="flex w-full flex-col self-end border border-t-2">{children}</div>;
+    return <div className="flex w-full flex-col self-end border-t-2">{children}</div>;
 }
