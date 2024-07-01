dfc5bf3719b565fc364f65c2004b0ef1d7b7ff5d
daichi904 <minimi.904.horo@gmail.com>
2024-07-01 16:30:17 +0900

    style: New style for todo list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
+import { Card, CardContent, CardTitle } from "@/components/elements/Card";
 import Modal from "@/components/elements/Modal";
 import Content from "@/components/layouts/Content";
 import Footer from "@/components/layouts/Footer";
@@ -10,7 +11,7 @@ import Page from "@/components/layouts/Page";
 import EditTodoModal from "@/features/routes/todo/EditTodoModal";
 import NewTodoModal from "@/features/routes/todo/NewTodoModal";
 import TodoDetailModal from "@/features/routes/todo/TodoDetailModal";
-import Todos from "@/features/routes/todo/Todos";
+import TodoList from "@/features/routes/todo/TodoList";
 import { useSetModal } from "@/hooks/useModals";
 import useResetTodosAtom from "@/hooks/useTodo";
 
@@ -19,24 +20,47 @@ export default function Home() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const reset = useResetTodosAtom();
 
+    // Todo一覧の高さの調整（全体の画面の縦幅が100vhになるようにする。）
+    const headerRef = useRef<HTMLDivElement>(null);
+    const footerRef = useRef<HTMLDivElement>(null);
+    const [headerHeight, setHeaderHeight] = useState(0);
+    const [footerHeight, setFooterHeight] = useState(0);
+    useEffect(() => {
+        if (headerRef.current === null || footerRef.current === null) return;
+        setHeaderHeight(headerRef.current.offsetHeight);
+        setFooterHeight(footerRef.current.offsetHeight);
+    }, []);
+
     return (
         <>
             <Page>
-                <Header>Header</Header>
+                <div ref={headerRef}>
+                    <Header>Header</Header>
+                </div>
                 <Content>
-                    <Todos />
+                    <div style={{ height: `calc(100vh - ${headerHeight}px - ${footerHeight}px)` }}>
+                        <TodoList />
+                    </div>
                 </Content>
-                <Footer>
-                    <button className="m-1 w-32 rounded-2xl bg-sky-600 px-2 py-1 shadow-xl" onClick={() => setIsModalOpen(true)}>
-                        <text className="text-xl text-white">Open</text>
-                    </button>
-                    <button className="m-1 w-32 rounded-2xl bg-sky-600 px-2 py-1 shadow-xl" onClick={() => setModal("newTodo")}>
-                        <text className="text-xl text-white">Create</text>
-                    </button>
-                    <button className="m-1 w-32 rounded-2xl bg-sky-600 px-2 py-1 shadow-xl" onClick={() => reset()}>
-                        <text className="text-xl text-white">Reset</text>
-                    </button>
-                </Footer>
+                <div ref={footerRef}>
+                    <Footer>
+                        <button
+                            className="m-1 w-32 rounded-2xl bg-sky-600 px-2 py-1 shadow-xl"
+                            onClick={() => setIsModalOpen(true)}
+                        >
+                            <text className="text-xl text-white">Open</text>
+                        </button>
+                        <button
+                            className="m-1 w-32 rounded-2xl bg-sky-600 px-2 py-1 shadow-xl"
+                            onClick={() => setModal("newTodo")}
+                        >
+                            <text className="text-xl text-white">Create</text>
+                        </button>
+                        <button className="m-1 w-32 rounded-2xl bg-sky-600 px-2 py-1 shadow-xl" onClick={() => reset()}>
+                            <text className="text-xl text-white">Reset</text>
+                        </button>
+                    </Footer>
+                </div>
             </Page>
 
             <NewTodoModal />
@@ -51,9 +75,24 @@ export default function Home() {
                     <Header>
                         <text>test</text>
                     </Header>
-                    <button className="m-1 w-32 rounded-2xl bg-sky-600 px-2 py-1 shadow-xl" onClick={() => setIsModalOpen(false)}>
-                        <text className="text-xl text-white">Close</text>
-                    </button>
+                    <Content>
+                        <Card>
+                            <CardTitle>Title</CardTitle>
+                            <CardContent>Content</CardContent>
+                        </Card>
+                        <Card>
+                            <CardTitle>Title</CardTitle>
+                            <CardContent>Content</CardContent>
+                        </Card>
+                    </Content>
+                    <Footer>
+                        <button
+                            className="m-1 w-32 rounded-2xl bg-sky-600 px-2 py-1 shadow-xl"
+                            onClick={() => setIsModalOpen(false)}
+                        >
+                            <text className="text-xl text-white">Close</text>
+                        </button>
+                    </Footer>
                 </Page>
             </Modal>
         </>
