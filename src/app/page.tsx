d31287da8fc86d912fd2b169b8db87aa7d69b580
Daichi Uematsu <minimi.904.horo@gmail.com>
2024-07-04 16:11:57 +0900

    feat: menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,12 @@ import Content from "@/components/layouts/Content";
 import Footer from "@/components/layouts/Footer";
 import Header from "@/components/layouts/Header";
 import Page from "@/components/layouts/Page";
+import Menu from "@/features/common/Menu";
 import EditTodoModal from "@/features/routes/todo/EditTodoModal";
 import NewTodoModal from "@/features/routes/todo/NewTodoModal";
 import TodoDetailModal from "@/features/routes/todo/TodoDetailModal";
 import TodoList from "@/features/routes/todo/TodoList";
+import { useControlMenu } from "@/hooks/useMenu";
 import { useSetModal } from "@/hooks/useModals";
 import useResetTodosAtom from "@/hooks/useTodo";
 
@@ -19,6 +21,8 @@ export default function Home() {
     const setModal = useSetModal();
     const [isModalOpen, setIsModalOpen] = useState(false);
     const reset = useResetTodosAtom();
+
+    const controlMenu = useControlMenu();
 
     // Todo一覧の高さの調整（全体の画面の縦幅が100vhになるようにする。）
     const headerRef = useRef<HTMLDivElement>(null);
@@ -35,7 +39,24 @@ export default function Home() {
         <>
             <Page>
                 <div ref={headerRef}>
-                    <Header>Header</Header>
+                    <Header>
+                        <div className="m-2 flex p-2" onClick={() => controlMenu(true)}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="size-6"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                                />
+                            </svg>
+                        </div>
+                    </Header>
                 </div>
                 <Content>
                     <div style={{ height: `calc(100vh - ${headerHeight}px - ${footerHeight}px)` }}>
@@ -56,9 +77,6 @@ export default function Home() {
                         >
                             <text className="text-xl text-white">Create</text>
                         </button>
-                        <button className="m-1 w-32 rounded-2xl bg-sky-600 px-2 py-1 shadow-xl" onClick={() => reset()}>
-                            <text className="text-xl text-white">Reset</text>
-                        </button>
                     </Footer>
                 </div>
             </Page>
@@ -68,6 +86,15 @@ export default function Home() {
             <TodoDetailModal />
 
             <EditTodoModal />
+
+            <Menu>
+                <button className="m-1 w-32 rounded-2xl bg-sky-600 px-2 py-1 shadow-xl" onClick={() => reset()}>
+                    <text className="text-xl text-white">Reset</text>
+                </button>
+                <button className="m-1 w-32 rounded-2xl bg-sky-600 px-2 py-1 shadow-xl" onClick={() => controlMenu(false)}>
+                    <text className="text-xl text-white">Close</text>
+                </button>
+            </Menu>
 
             {/**This is experimental modal */}
             <Modal isOpen={isModalOpen}>
