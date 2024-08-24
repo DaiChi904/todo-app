55ac37af2b73b6e04bedbeeea5e960d6651308e7
daichi904 <minimi.904.horo@gmail.com>
2024-08-25 00:41:34 +0900

    feat: Added feature to sort and mark isChecked

--- src/features/routes/todo/checkBox/index.tsx
+++ src/features/routes/todo/checkBox/index.tsx
@@ -1,9 +1,10 @@
 import { SetStateAction } from "jotai";
-import { ChangeEvent, Dispatch, useState } from "react";
+import { Dispatch, useState } from "react";
 
 import { CheckList } from "@/hooks/useTodo";
 
-import { Bars_3, CheckBadge, CheckBadgeSolid, Trash } from "../../../../../public/HeroiconsSVGs";
+import NomalView from "./NomalView";
+import SortView from "./SortView";
 
 interface Props {
     checkList: CheckList[];
@@ -11,72 +12,32 @@ interface Props {
 }
 
 export default function CheckBox({ checkList, setCheckList }: Props) {
-    const [isConfirmed, setIsConfirmed] = useState<boolean>(true);
-    const [content, setContent] = useState("");
-    const handleTitleInput = (e: ChangeEvent<HTMLInputElement>) => {
-        setContent(e.target.value);
-    };
-
-    const handleConfirm = () => {
-        const id = "listid" + Math.random().toString(12).slice(2);
-        setCheckList([...checkList, { id: id, content: content, isChecked: false }]);
-        setContent("");
-        setIsConfirmed(true);
-    };
-
-    const handleDelete = (target: CheckList) => {
-        const pendingCheckList = checkList.filter((element) => element.id !== target.id);
-        setCheckList(pendingCheckList);
-    };
+    const [isSorting, setIsSorting] = useState<boolean>(false);
 
     return (
         <>
             <text>Check List</text>
-            <div className="flex w-full flex-col">
-                {checkList?.map((element) => (
-                    <div key={element.id} className="my-1 flex flex-row items-center">
-                        <div className="m-1">
-                            <Bars_3 />
-                        </div>
-                        <div>{element.isChecked ? <CheckBadgeSolid /> : <CheckBadge />}</div>
-                        <div className="ml-1">
-                            <text className="break-all">{element.content}</text>
-                        </div>
-                        <div className="m-1" onClick={() => handleDelete(element)}>
-                            <Trash />
-                        </div>
-                    </div>
-                ))}
-                {isConfirmed ? (
-                    <button className="m-1 size-fit rounded-full bg-sky-600 p-2 shadow-2xl" onClick={() => setIsConfirmed(false)}>
-                        Add List
+            {!isSorting ? (
+                <>
+                    <NomalView checkList={checkList} setCheckList={setCheckList} />
+                    <button
+                        className="m-1 size-fit rounded-full bg-sky-600 p-2 shadow-2xl"
+                        onClick={() => setIsSorting(!isSorting)}
+                    >
+                        Start Sorting
                     </button>
-                ) : (
-                    <>
-                        <input
-                            name="title"
-                            className="h-min resize-none items-center rounded-2xl border-2 border-sky-600/60 p-2 text-justify text-xl shadow-2xl"
-                            value={content}
-                            onChange={(e) => handleTitleInput(e)}
-                            maxLength={80}
-                        />
-                        <div className="flex flex-row">
-                            <button
-                                className="m-1 size-fit rounded-full bg-sky-600 p-2 shadow-2xl"
-                                onClick={() => handleConfirm()}
-                            >
-                                Confirm
-                            </button>
-                            <button
-                                className="m-1 size-fit rounded-full bg-sky-600 p-2 shadow-2xl"
-                                onClick={() => setIsConfirmed(true)}
-                            >
-                                Cancel
-                            </button>
-                        </div>
-                    </>
-                )}
-            </div>
+                </>
+            ) : (
+                <>
+                    <SortView checkList={checkList} setCheckList={setCheckList} />
+                    <button
+                        className="m-1 size-fit rounded-full bg-sky-600 p-2 shadow-2xl"
+                        onClick={() => setIsSorting(!isSorting)}
+                    >
+                        End Sorting
+                    </button>
+                </>
+            )}
         </>
     );
 }
