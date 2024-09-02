e54a9122280cc146df8c260927bf4d16b274d5a7
daichi904 <minimi.904.horo@gmail.com>
2024-09-02 22:07:58 +0900

    minor fix

--- src/components/elements/Modal.tsx
+++ src/components/elements/Modal.tsx
@@ -10,11 +10,13 @@ interface Props {
 export default function Modal({ children, isOpen }: Props) {
     const bodyRef = useRef(document.body.style.overflow);
     useEffect(() => {
-        if (isOpen) {
-            bodyRef.current = document.body.style.overflow;
-            document.body.style.overflow = "hidden";
-        } else {
-            document.body.style.overflow = "visible";
+        if (typeof window !== "undefined") {
+            if (isOpen) {
+                bodyRef.current = document.body.style.overflow;
+                document.body.style.overflow = "hidden";
+            } else {
+                document.body.style.overflow = "visible";
+            }
         }
     }, [isOpen]);
 
